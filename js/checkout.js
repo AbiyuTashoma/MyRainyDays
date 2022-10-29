ba4c603b5853b297a739f274d31d0b12d0c76ea3
AbiyuTashoma <85228220+AbiyuTashoma@users.noreply.github.com>
2022-10-29 10:04:53 +0200

    Added shipping validation
    Added shipping validation
    Added country list

--- js/checkout.js
+++ js/checkout.js
@@ -76,6 +76,15 @@ const noteLastNameContainer = document.querySelector(".note-lastname");
 const noteMobileContainer = document.querySelector(".note-mobile");
 const noteEmailContainer = document.querySelector(".note-email");
 
+//shipping information
+const streetAddressContainer = document.querySelector("#street-address");
+const postalCodeContainer = document.querySelector("#postal-code");
+const cityContainer = document.querySelector("#city");
+
+const noteStreetAddressContainer = document.querySelector(".note-streetaddress");
+const notePostalCodeContainer = document.querySelector(".note-postalcode");
+const noteCityContainer = document.querySelector(".note-city");
+
 //Payment information
 const cardNumberContainer = document.querySelector("#card-number");
 const validMonthContainer = document.querySelector("#valid-month");
@@ -91,14 +100,7 @@ const noteCVCContainer = document.querySelector(".note-cvc");
 const noteVippsNumberContainer = document.querySelector(".note-vippsnumber");
 
 //clear error message onchange and oninput
-visaPaymentRadio.onchange = function() {
-    clearVippsError();
-}
-
-vippsPaymentRadio.onchange = function() {
-    clearVisaError();
-}
-
+//personel information
 firstNameContainer.oninput = function() {
     clearError(noteFirstNameContainer, firstNameContainer);
 }
@@ -113,6 +115,30 @@ mobileContainer.oninput = function() {
 
 emailContainer.oninput = function() {
     clearError(noteEmailContainer, emailContainer);
+}
+
+//clear error message onchange and oninput
+//shipping information
+streetAddressContainer.oninput = function() {
+    clearError(noteStreetAddressContainer, streetAddressContainer);
+}
+
+postalCodeContainer.oninput = function() {
+    clearError(notePostalCodeContainer, postalCodeContainer);
+}
+
+cityContainer.oninput = function() {
+    clearError(noteCityContainer, cityContainer);
+}
+
+//clear error message onchange and oninput
+//payment information
+visaPaymentRadio.onchange = function() {
+    clearVippsError();
+}
+
+vippsPaymentRadio.onchange = function() {
+    clearVisaError();
 }
 
 cardNumberContainer.oninput = function() {
@@ -154,6 +180,10 @@ function validate(event) {
     const mobile = mobileContainer.value;
     const email = emailContainer.value;
 
+    const streeAddress = streetAddressContainer.value;
+    const postalCode = postalCodeContainer.value;
+    const city = cityContainer.value;
+
     const cardNumber = cardNumberContainer.value;
     const validMonth = validMonthContainer.value;
     const validYear = validYearContainer.value;
@@ -167,13 +197,17 @@ function validate(event) {
     const validMobile = validateNumber(mobile);
     const validEmail = validateEmail(email);
 
+    const validStreetAddress = validateText(streeAddress, 5);
+    const validPostalCode = validateNumber(postalCode, 4);
+    const validCity = validateText(city, 2);
+
     const validCardNumber = validateNumber(cardNumber, 16);
     const validValidMonth = validateMonth(validMonth);
     const validValidYear = validateNumber(validYear, 2);
     const validValidCVC = validateNumber(validCVC, 3);
     const validValidVipps = validateNumber(validVipps);
 
-// Checkout
+    // Checkout
     // if (!checkedSeat && (selectedEvent === "aeronautics_lecture")) {
     //     validCheckout = false;
     //     setError(noteSelectSeat, roomContainer, "Select Seat");
@@ -184,7 +218,46 @@ function validate(event) {
     //     setError(noteQuantityContainer, quantityContainer, "Enter valid reservation");
     // }
 
-// payment
+
+    //personal info
+
+    if (!validFirstName) {
+        validCheckout = false;
+        setError(noteFirstNameContainer, firstNameContainer, "First name is required");
+    }
+
+    if (!validLastName) {
+        validCheckout = false;
+        setError(noteLastNameContainer, lastNameContainer, "Last name is required");
+    }
+
+    if (!validMobile) {
+        validCheckout = false;
+        setError(noteMobileContainer, mobileContainer, "Enter valid Phone number");
+    }
+
+    if (!validEmail) {
+        validCheckout = false;
+        setError(noteEmailContainer, emailContainer, "Enter valid email");
+    }
+
+    //shipping
+    if (!validStreetAddress) {
+        validCheckout = false;
+        setError(noteStreetAddressContainer, streetAddressContainer, "Enter valid street address");
+    }
+
+    if (!validPostalCode) {
+        validCheckout = false;
+        setError(notePostalCodeContainer, postalCodeContainer, "Enter valid postal code");
+    }
+
+    if (!validCity) {
+        validCheckout = false;
+        setError(noteCityContainer, cityContainer, "Enter valid city");
+    }
+
+    //payment
     if (!validCardNumber && visaChecked) {
         validCheckout = false;
         setError(noteCardNumberContainer, cardNumberContainer, "Enter valid card number");
@@ -210,28 +283,7 @@ function validate(event) {
         setError(noteVippsNumberContainer, validVippsNumberContainer, "Enter valid phone number");
     }
 
-//personal info
-
-    if (!validFirstName) {
-        validCheckout = false;
-        setError(noteFirstNameContainer, firstNameContainer, "First name is required");
-    }
-
-    if (!validLastName) {
-        validCheckout = false;
-        setError(noteLastNameContainer, lastNameContainer, "Last name is required");
-    }
-
-    if (!validMobile) {
-        validCheckout = false;
-        setError(noteMobileContainer, mobileContainer, "Enter valid Phone number");
-    }
-
-    if (!validEmail) {
-        validCheckout = false;
-        setError(noteEmailContainer, emailContainer, "Enter valid email");
-    }
-
+    //on checkout
     if (validCheckout) {
         messageContainer.innerHTML = displayMessage("Order successfully completed!", "success");
         checkoutFormContainer.reset();
