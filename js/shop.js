e7049cecd6e594138e7a0e0f53515ae5140ebb62
AbiyuTashoma <85228220+AbiyuTashoma@users.noreply.github.com>
2022-11-06 22:08:25 +0100

    added order by feature
    added order by feature

--- js/shop.js
+++ js/shop.js
@@ -1,10 +1,9 @@
 const productsContainer = document.querySelector(".collection-container");
 
+const orderByContainer = document.querySelector(".orderby");
+
 const url = "https://www.rainydays.casa/wp-json/wc/store/products";
 
-
-// orderby id and order ascending
-// https://www.rainydays.casa/wp-json/wc/store/products/?orderby=id&order=asc
 
 async function getProducts() {
 
@@ -23,3 +22,26 @@ async function getProducts() {
 }
 
 getProducts();
+
+// orderby id and order ascending
+// https://www.rainydays.casa/wp-json/wc/store/products/?orderby=id&order=asc
+// orderby=price asc and desc
+// https://www.rainydays.casa/wp-json/wc/store/products/?orderby=price&order=desc
+// date, price, rating, popularity, modified, id, include, title, slug, menu_order, comment_count
+
+orderByContainer.onchange = async function () {
+    const orderBy = orderByContainer.value;
+
+    const newURL = `https://www.rainydays.casa/wp-json/wc/store/products/?orderby=${orderBy}`;
+
+    try {
+        const response = await fetch(newURL);
+        const newList = await response.json();
+
+        productsContainer.innerHTML = displayProducts(newList);
+    }
+
+    catch (error) {
+        productsContainer.innerHTML = displayMessage("An error has occurred. Please try again", "error");
+    }
+}
