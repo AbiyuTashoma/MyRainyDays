c1f242a27d915071de1bf9efee22015088df620e
AbiyuTashoma <85228220+AbiyuTashoma@users.noreply.github.com>
2022-11-12 23:34:01 +0100

    Replaced add to cart with view
    Replaced add to cart with view
    Code cleanup

--- js/components/shopHtml.js
+++ js/components/shopHtml.js
@@ -1,14 +1,18 @@
+// const addToCartHTML = `<div>
+//                             <input type="button" class="add-to-cart" id="addtocart">
+//                             <label for="addtocart" title="add to cart">
+//                                 <div class="add-to-cart-shop">
+//                                     <div class="cart-container">
+//                                         <img src="icons/cart.svg" alt="cart" class="cartsize-shop">
+//                                         <p class="add-cart">+</p>
+//                                     </div>
+//                                     <p class="add-to-cart-txt-shop">Add To Cart</p>
+//                                 </div>
+//                             </label>
+//                        </div>`;
+
 const addToCartHTML = `<div>
-                            <input type="submit" class="add-to-cart" id="addtocart">
-                            <label for="addtocart" title="add to cart">
-                                <div class="add-to-cart-shop">
-                                    <div class="cart-container">
-                                        <img src="icons/cart.svg" alt="cart" class="cartsize-shop">
-                                        <p class="add-cart">+</p>
-                                    </div>
-                                    <p class="add-to-cart-txt-shop">Add To Cart</p>
-                                </div>
-                            </label>
+                            <input type="button" class="view-product" id="viewproduct" value="View">
                         </div>`;
                         
 function displayProducts (products) {
@@ -19,14 +23,16 @@ function displayProducts (products) {
                             <img src="${products[i]["images"][0]["src"]}" class="grid-image" alt="hiking jacket woman">                                                
                         </a>
                         <div class="product-footer">
-                            <div>                                
+                            <div class="title-container">                                
                                 <a href="product.html?productID=${products[i]["id"]}" class="a-title">
                                     <p class="p-title">${products[i]["name"]}</p>
                                     <p class="p-price">${products[i]["price_html"]}</p>
                                 </a>
                             </div>
-                            ${addToCartHTML}
-                        </div>
+                                <a href="product.html?productID=${products[i]["id"]}" class="a-title">
+                                    ${addToCartHTML}
+                                </a>
+                            </div>
                     </div>`;     
     }
 
